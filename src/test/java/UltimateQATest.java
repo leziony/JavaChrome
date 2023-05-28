55d13648ee1fc6ba9362ff2024e907cc225d5479
Mateusz Kowalczewski <59838505+leziony@users.noreply.github.com>
2023-05-28 14:49:43 +0200

    Update UltimateQATest.java

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.Sleeper;
 
 public class UltimateQATest extends PageSetup{
 
+    //tester
     @Test
     public void testButton() throws InterruptedException {
         WebElement tester = driver.findElementById("idExample");
@@ -21,11 +22,11 @@ public class UltimateQATest extends PageSetup{
         WebElement tester2 = driver.findElementByName("et_pb_contact_email_0");
         WebElement tester3 = driver.findElementByName("et_builder_submit_button");
         tester1.sendKeys("Mati");
-        Thread.sleep(500);
+        Thread.sleep(600);
         tester2.sendKeys("matmat@example.com");
-        Thread.sleep(500);
+        Thread.sleep(600);
         tester3.click();
-        Thread.sleep(2000);
+        Thread.sleep(2250);
         WebElement success = driver.findElementByClassName("et-pb-contact-message");
         Assertions.assertEquals("Thanks for contacting us",success.getText());
 
