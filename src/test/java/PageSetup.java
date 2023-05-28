be5ab81f5e4ede365f8904a2eb26fe6822b2633e
Mateusz Kowalczewski <mati.k5@onet.pl>
2023-05-28 14:05:36 +0200

    Function Test

--- src/test/java/PageSetup.java
+++ src/test/java/PageSetup.java
@@ -4,16 +4,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PageSetup {
 
-    private String projectLocation = System.getProperty("user.dir");
-    private ChromeDriver driver;
+    String projectLocation = System.getProperty("user.dir");
+    ChromeDriver driver;
 
     @BeforeEach
     public void startSession() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver",projectLocation + "/Resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://ultimateqa.com/automation/");
-        Thread.sleep(1500);
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+        Thread.sleep(500);
     }
 
     @AfterEach
--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -1,16 +1,18 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Sleeper;
 
 public class UltimateQATest extends PageSetup{
 
     @Test
-    public void testButton()
-    {
-
+    public void testButton() throws InterruptedException {
+        WebElement tester = driver.findElementById("idExample");
+        tester.click();
+        Thread.sleep(500);
+        WebElement success = driver.findElementByClassName("entry-title");
+        Thread.sleep(500);
+        Assertions.assertEquals("Button success",success.getText());
     }
 
 
