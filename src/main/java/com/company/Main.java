f2ae18b26d8bf018b386c18c0c4634a15a1ec40f
Mateusz Kowalczewski <mati.k5@onet.pl>
2023-05-28 15:38:44 +0200

    Main

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Complete!");
+        int i = 0;
+        i++;
+        System.out.print("Yay!");
     }
 }
